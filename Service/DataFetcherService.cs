55cb7ccc9b5608ebc5c2169fec54494b64ede963
Kuba <kuba.miskiewicz01@gmail.com>
2025-03-23 13:38:51 +0100

    Added fetching avaliable tickers from 2 apis including their share price and dividend yield

--- Service/DataFetcherService.cs
+++ Service/DataFetcherService.cs
@@ -1,4 +1,10 @@
-﻿namespace PortfolioTracker.Service
+﻿using Newtonsoft.Json.Linq;
+using PortfolioTracker.Model;
+using PortfolioTracker.PortfolioDbContext;
+using System.Globalization;
+using System.Text.Json.Nodes;
+
+namespace PortfolioTracker.Service
 {
     public class DataFetcherService
     {
@@ -13,42 +19,87 @@
 
         public async Task FetchAllAvaliableTickers()
         {
-            var client = new HttpClient();
-            Console.WriteLine(_configuration["AlphaVentageBaseUrl"]);
-            client.BaseAddress = new Uri(_configuration["AlphaVentageBaseUrl"]);
             _logger.LogInformation("Fetching data");
+            var client = new HttpClient() { BaseAddress = new Uri(_configuration["AlphaVentageBaseUrl"]) };
             var response = await client.GetAsync(
                 "/query?function=LISTING_STATUS&apikey=" + _configuration["AlphaVentageApiKey"]
             );
 
             if (response.IsSuccessStatusCode)
             {
-                var csvData = await response.Content.ReadAsStringAsync();
-                var tickers = ParseCsv(csvData);
-                _logger.LogInformation("Fetched tickers: {Tickers}", tickers);
+                var context = new PortfolioTrackerContext(); ;
+                var data = await response.Content.ReadAsStringAsync();
+                var tickerSymbol = ReadRowFromCsv(data, 0);
+                var companyNames = ReadRowFromCsv(data, 1);
+               
+                foreach (var item in tickerSymbol)
+                {
+                    var doesTickerExist = context.tickers.Any(x => x.TickerSymbol.Equals(item));
+                    if (doesTickerExist == false)
+                    {
+                        var newTicker = new Ticker()
+                        {
+                            CompanyName = companyNames[tickerSymbol.IndexOf(item)],
+                            DividendPerShare = await getNumericalInformationFromMetrics(item, "dividendPerShareAnnual"),
+                            DividendYield = await getNumericalInformationFromMetrics(item, "dividendYieldIndicatedAnnual"),
+                            SharePrice = await getCurrentSharePrice(item),
+                            TickerSymbol = item,
+                            LastUpdateDate = DateTime.Now,
+                        };
+                        context.tickers.Add(newTicker);
+                        await context.SaveChangesAsync();
+                        await Task.Delay(TimeSpan.FromSeconds(10)); // wait so the we dont get 429
+                    }
+                    else
+                    {
+                        _logger.LogInformation($"ticker {item} has been already saved");
+                    }
+                }
+                context.Dispose();
             }
             else
             {
-                _logger.LogError("Failed to fetch tickers. Status code: {StatusCode}", response.StatusCode);
+                _logger.LogError("Failed to fetch tickers");
             }
+
+            _logger.LogInformation("Fetching finished");
         }
 
-        private List<string> ParseCsv(string csvData)
+        private async Task<decimal> getNumericalInformationFromMetrics(string companyTicker, string information)
         {
-            var tickers = new List<string>();
+            var client = new HttpClient() { BaseAddress = new Uri(_configuration["FinnhubBaseUrl"]) };
+            var response = await client.GetFromJsonAsync<JsonObject>($"/api/v1/stock/metric?symbol={companyTicker}&token="
+                + _configuration["FinnhubApiKey"]);
+
+            Console.WriteLine(response);
+            return response["metric"][$"{information}"] != null ? decimal.Parse(response["metric"][$"{information}"].ToString(), CultureInfo.InvariantCulture) : 0.0m;
+        }
+
+        private async Task<decimal> getCurrentSharePrice(string companyTicker)
+        {
+            var client = new HttpClient() { BaseAddress = new Uri(_configuration["FinnhubBaseUrl"]) };
+            var response = await client.GetFromJsonAsync<JsonObject>($"/api/v1/quote?&symbol={companyTicker}&token="
+                + _configuration["FinnhubApiKey"]);
+            Console.WriteLine(response);
+            return decimal.Parse(response["c"].ToString(), CultureInfo.InvariantCulture);
+        }
+
+        private List<string> ReadRowFromCsv(string csvData, int row)
+        {
+            var dataExtracted = new List<string>();
             using (var reader = new StringReader(csvData))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     var values = line.Split(',');
-                    if (values.Length > 0 && values[6].ToLower()=="active")
+                    if (values.Length > 0 && values[6].ToLower() == "active")
                     {
-                        tickers.Add(values[0]);
+                        dataExtracted.Add(values[row]);
                     }
                 }
             }
-            return tickers;
+            return dataExtracted;
         }
-    }
+    } 
 }
